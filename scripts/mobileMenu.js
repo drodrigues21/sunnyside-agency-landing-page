41f2ff7195b272ac4340877f2e95afc355c97947
David Rodrigues <davidvilar21@gmail.com>
2021-10-02 02:39:59 +0900

    fix ui bugs

--- scripts/mobileMenu.js
+++ scripts/mobileMenu.js
@@ -1,13 +1,13 @@
 // Hamburger mane toggle
 const toggleButton = document.querySelector('.container');
-const navLinks = document.querySelector('.nav-links-container');
+const mobileNav = document.querySelector('.mobile-nav');
 
 toggleButton.addEventListener('click', (e) => {
     const toggle = e.target;
     
     if(toggle.classList.contains('hamburger-menu') ||toggle.classList.contains('hamburger-menu--img')) {
-       navLinks.classList.toggle('active');
+        mobileNav.classList.toggle('active');
     } else {
-        navLinks.classList.remove('active');
+        mobileNav.classList.remove('active');
     }
 });
